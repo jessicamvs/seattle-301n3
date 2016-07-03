24109a3905afe1e2920389f443aa2e235cd494de
geoffreycc <gcox3d@gmail.com>
2016-07-03 09:55:34 -0700

    aboutController.js todo done

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -1,5 +1,7 @@
 (function(module) {
   var aboutController = {};
+  $('.tab-content').hide();
+  $('#about').show();
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
