9e82853d46ff71ecab4b608858e582f45078bf2d
geoffreycc <gcox3d@gmail.com>
2016-07-03 09:53:13 -0700

    Completed to-do's in articleControler.js

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -2,13 +2,15 @@
   var articlesController = {};
   Article.createTable();
   Article.fetchAll(articleView.initIndexPage);
+  $('.tab-content').hide();
+  $('#articles').show();
 
 
-  // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
 
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
-  // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
+  // DONE: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
 
   };
