25bb35639ba2c2ded935c139426ab445be0e750e
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-07-06 19:17:42 -0700

    Did ajax request for repos

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -4,8 +4,19 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/users/jessicamvs/repos',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + GITHUB_TOKEN},
+      success: function(data) {
+        data.forEach(function(ele) {
+          repos.all.push(ele);
+        })
+      }
+    })
 
+    callback();
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
